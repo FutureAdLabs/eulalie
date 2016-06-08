1745d1e64ee756e656f56e6e3e16ce50dfa32c4f
Andy Williams <andy.williams@adludio.com>
2016-06-08 11:09:01 +0100

    Minor fix

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import chalk from "chalk";
 
 function isGen(v) {
   try {
-    if (typeof regeneratorRuntime !== undefined) {
+    if (typeof regeneratorRuntime !== "undefined") {
       /* global regeneratorRuntime */
       return regeneratorRuntime.isGeneratorFunction(v);
     }
